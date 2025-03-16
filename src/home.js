23e51973b7f6c1ca762b0ed4a0a567a6e5132b50
innkeep <theMagicalinnkeeper@proton.me>
2025-03-16 10:18:07 +0100

    Modifies CSS and Home.js

--- src/home.js
+++ src/home.js
@@ -1,5 +1,13 @@
 const homeContent = document.createElement("div");
 homeContent.setAttribute("class","content");
-homeContent.textContent="Home?";
 
+const contHeader = document.createElement("div");
+contHeader.setAttribute("class","title");
+contHeader.textContent="Home";
+
+const contBody = document.createElement("div");
+contBody.setAttribute("class","body");
+
+homeContent.appendChild(contHeader);
+homeContent.appendChild(contBody);
 export {homeContent};
